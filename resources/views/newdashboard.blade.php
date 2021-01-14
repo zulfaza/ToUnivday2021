8e51da85e7b72a7b8b0da4cf2b15d3d866f5fae7
zulfaza <32305083+zulfaza@users.noreply.github.com>
2021-01-15 02:26:23 +0700

    add mobile dashboard

--- resources/views/newdashboard.blade.php
+++ resources/views/newdashboard.blade.php
@@ -1,60 +1,66 @@
 <x-app-layout>
-    <div class="container">
-        <div class="grid grid-rows-4 grid-cols-7 grid-flow-col gap-10 h-screen py-4">
+    <div class="container px-6">
+        <div class="lg:grid grid-rows-4 grid-cols-7 grid-flow-col gap-10 h-screen py-4">
             <div class="col-span-1 row-span-full">
-                <nav class="bg-pink-atas px-3 py-4 rounded-2xl h-full flex items-center justify-between flex-col">
-                    <div>
-                        <div class="flex justify-center mb-20 mt-5">
+                <nav 
+                    class="navbar w-11/12">
+                    <div class="w-full">
+                        <div class="lg:flex justify-center mb-20 mt-5 hidden">
                             <h5 class="text-white font-bold text-3xl border-b-4 border-white">
                                 dash
                             </h5>
                         </div>
-                        <ul>
-                            <li class="p-3 rounded-xl flex justify-center items-center mb-2 hover:bg-white hover:bg-opacity-30 transition">
+                        <ul class="flex flex-row justify-center lg:flex-col">
+                            <li class="nav-item">
                                 <a href="/">
                                     <img src="{{asset('/img/Icon/home.svg')}}" alt="home">
                                 </a>
                             </li>
-                            <li class="p-3 rounded-xl flex justify-center items-center mb-2 hover:bg-white hover:bg-opacity-30 transition">
+                            <li class="nav-item">
                                 <a href="/">
                                     <img src="{{asset('/img/Icon/menu.svg')}}" alt="home">
                                 </a>
                             </li>
-                            <li class="p-3 rounded-xl flex justify-center items-center mb-2 hover:bg-white hover:bg-opacity-30 transition">
+                            <li class="nav-item">
                                 <a href="/">
                                     <img src="{{asset('/img/Icon/edit.svg')}}" alt="home">
                                 </a>
                             </li>
-                            <li class="p-3 rounded-xl flex justify-center items-center mb-2 hover:bg-white hover:bg-opacity-30 transition">
+                            <li class="nav-item">
                                 <a href="/">
                                     <img src="{{asset('/img/Icon/other.svg')}}" alt="home">
                                 </a>
                             </li>
+                            <li class="nav-item block lg:hidden">
+                                <a href="{{route('dashboard')}}">
+                                    <img src="{{asset('/img/Icon/profile white.svg')}}" alt="home">
+                                </a>
+                            </li>
                         </ul>
                     </div>
-                    <div>
+                    <div class="hidden lg:block">
                         <img src="{{asset('/img/Logo festum.png')}}" alt="">
                     </div>
                 </nav>
             </div>
             <div class="col-span-4 row-span-2 pb-10">
-                <div class="relative h-full pt-36">
+                <div class="relative h-full lg:pt-36">
                     <div class="bg-pink-atas rounded-xl h-full w-full">
 
                     </div>
-                    <div class="flex flex-row absolute bottom-0">
-                        <img class="h-full max-h-96 w-auto" src="{{asset('img/Dashboard/Saly-19.png')}}" alt="ilustrasi">
-                        <div class="self-end mb-24 text-white">
-                            <h2 class="font-bold text-4xl" >selamat datang</h2>
-                            <h3 class="mt-3 text-xl">di halaman depan try out 
+                    <div class="flex flex-col-reverse lg:flex-row bg-pink-atas lg:bg-opacity-0 rounded-2xl lg:rounded-none relative lg:absolute bottom-0 pt-5 px-3 lg:py-0">
+                        <img class="h-auto w-full lg:w-3/5" src="{{asset('img/Dashboard/Saly-19.png')}}" alt="ilustrasi">
+                        <div class="hidden self-end lg:block mb-14 text-white">
+                            <h2 class="font-bold text-xl sm:text-2xl" >selamat datang</h2>
+                            <h3 class="mt-3 lg:text-base">di halaman depan try out 
                                 smansaunivday 2021 !!!</h3>
                             </div>
                         </div>
                 </div>
             </div>
             <div class="col-span-4 row-span-2">
-                <div class="w-full h-full py-5">
-                    <h1 class="font-bold text-6xl mb-10" >smansaunivday</h1>
+                <div class="w-full h-full py-5 text-center lg:text-left">
+                    <h1 class="font-bold text-3xl md:text-4xl lg:text-6xl mb-10" >smansaunivday</h1>
                     <p>univday tahun ini bakalan sedikit beda dibanding tahun tahun
                         sebelumnya. ya mau begimana lagi ? pandeminya masih ada,
                         tapi emang sih, di era disrupsi kek gini, kita semua tuh harus
@@ -68,7 +74,7 @@
             </div>
             <div class="col-span-2 row-span-full">
 
-                <div class="my-4">
+                <div class="hidden lg:block mb-4">
                     <div class="flex justify-end items-center">
                         <div class="nama mr-3 ">
                             <a class="text-xl text-black hover:text-pink-atas transition-colors" href="{{route('dashboard')}}">
@@ -83,9 +89,9 @@
                     </div>
                 </div>
 
-                <div class="h-3/4 w-full">
+                <div class="h-full lg:h-3/4 w-full">
                     <div 
-                    class="bg-pink-atas font-bold text-white text-center h-full w-full rounded-2xl px-6 py-8 flex flex-col items-center bg-bottom bg-no-repeat" 
+                    class="bg-pink-atas font-bold text-white text-center h-full w-full rounded-2xl px-6 py-8 flex flex-col items-center bg-bottom bg-no-repeat pb-72 lg:pb-0" 
                     style="background-image: url('/img/Dashboard/Intersect.png');"
                     >
                         <div class="bg-blue py-2 px-4 text-white text-2xl shadow-md w-max rounded-xl">
